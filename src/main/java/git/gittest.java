8b6c3539ea91c4267a340336c28dc700e112472d
George <12134@qq.com>
2022-08-02 16:46:14 +0800

    第hot-fix22次提交

--- src/main/java/git/gittest.java
+++ src/main/java/git/gittest.java
@@ -4,6 +4,7 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("赖明华是个带帅哥哥");
         System.out.println("赖明华是ge chao ji wu di da shuai ge 哥哥");
+        System.out.println("赖明华是ge哥哥");
 
     }
 
